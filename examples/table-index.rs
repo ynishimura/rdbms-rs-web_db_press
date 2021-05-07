4cadf6a29604b9bf3ddf3d88777238dcf5267a6a
ynishimura <ynishimura0922@gmail.com>
2021-05-07 23:51:26 +0900

    add query

--- examples/table-index.rs
+++ examples/table-index.rs
@@ -2,21 +2,21 @@ use anyhow::Result;
 
 use rdbms_rs_web_db_press::buffer::{BufferPool, BufferPoolManager};
 use rdbms_rs_web_db_press::disk::{DiskManager, PageId};
-use rdbms_rs_web_db_press::query::{Filter, PlanNode, SeqScan, TupleSearchMode};
+use rdbms_rs_web_db_press::query::{IndexScan, PlanNode, TupleSearchMode};
 use rdbms_rs_web_db_press::tuple;
 
+// SELECT * WHERE last_name = 'Smith'
+// with index
 fn main() -> Result<()> {
-    let disk = DiskManager::open("simple.rly")?;
+    let disk = DiskManager::open("table.rly")?;
     let pool = BufferPool::new(10);
     let mut bufmgr = BufferPoolManager::new(disk, pool);
 
-    let plan = Filter {
-        cond: &|record| record[1].as_slice() < b"Dave",
-        inner_plan: &SeqScan {
-            table_meta_page_id: PageId(0),
-            search_mode: TupleSearchMode::Key(&[b"w"]),
-            while_cond: &|pkey| pkey[0].as_slice() < b"z",
-        },
+    let plan = IndexScan {
+        table_meta_page_id: PageId(0),
+        index_meta_page_id: PageId(2),
+        search_mode: TupleSearchMode::Key(&[b"Smith"]),
+        while_cond: &|skey| skey[0].as_slice() == b"Smith",
     };
     let mut exec = plan.start(&mut bufmgr)?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod btree;
 pub mod buffer;
 pub mod disk;
 mod memcmpable;
-// pub mod query;
+pub mod query;
 mod slotted;
 pub mod table;
 pub mod tuple;
